0fb1fae53f0a7edc206e50d4e2cf3b83e8ff77c7
DinitiWelivita <47827857+DinitiWelivita@users.noreply.github.com>
2020-10-16 10:21:05 -0400

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,54 @@
+package hardestgame;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author 802967
+ */
+public class Player {
+    //declared, not initialized
+    private int x, y;
+    //declared and initialized
+    //static means all Players
+    //final means cant change, constant
+    private static final int speed = 3;
+    private static final int WIDTH = 30, HEIGHT = 30;
+    private static final Color COLOR = Color.RED;
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public void draw(Graphics g) {
+        g.setColor(COLOR);
+        g.fillRect(x, y, WIDTH, HEIGHT);
+        g.setColor(Color.BLACK);
+        g.fillRect(x, y, WIDTH, HEIGHT);
+    }
+    
+    public void move(int dx, int dy) {
+        x += dx * speed;
+        y += dy * speed;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public static int getWidth() {
+        return WIDTH;
+    }
+
+    public static int getHeight() {
+        return HEIGHT;
+    }
+    
+    
+}
