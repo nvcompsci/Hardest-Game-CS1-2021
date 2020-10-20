6385ef5220a25fe782bed58a7f77aa219f45c1de
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-10-20 14:45:38 -0400

    challenge solutions

--- Enemy.java
+++ Enemy.java
@@ -5,10 +5,50 @@
  */
 package hardestgame;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 /**
  *
  * @author jword
  */
 public class Enemy {
+    private int x, y, vx, vy;
+    private final static int SPEED = 2;
+    private final static int WIDTH = 25, HEIGHT = 25;
+    private final static Color COLOR = Color.BLUE;
+    
+    //4.a constructor
+    public Enemy(int x, int y, int vx, int vy) {
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+    }
+    
+    //4.b draws a circle (oval)
+    public void draw(Graphics g) {
+        g.setColor(COLOR);
+        g.fillOval(x, y, WIDTH, HEIGHT);
+        g.setColor(Color.BLACK);
+        g.drawOval(x, y, WIDTH, HEIGHT);
+    }
+    //4.c getters
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public static int getWIDTH() {
+        return WIDTH;
+    }
+
+    public static int getHEIGHT() {
+        return HEIGHT;
+    }
     
 }
--- Player.java
+++ Player.java
@@ -5,31 +5,25 @@ import java.awt.Graphics;
 
 /**
  *
- * @author jword
+ * @author John Word
  */
 public class Player {
-    //declared, not initialized
     private int x, y;
-    //declared and initialized
-    //static means all Players
-    //final means cannont change, constant
-    private static final int speed = 3;
-    private static final int width = 30, height = 30;
-    private static final Color color = new Color(0xEE0088);
+    //constants
+    private final static int WIDTH = 50, HEIGHT = 50;
+    private final static Color COLOR = Color.RED;
 
     public Player(int x, int y) {
         this.x = x;
         this.y = y;
     }
     
-    public void draw(Graphics g) {
-        g.setColor(color);
-        g.fillRect(x, y, width, height);
-    }
-    
-    public void move(int dx, int dy) {
-        x += dx * speed;
-        y += dy * speed;
+    //1. draw outline on shape
+    public void draw(Graphics g) {        
+        g.setColor(COLOR);
+        g.fillRect(x, y, WIDTH, HEIGHT);
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, WIDTH, HEIGHT);
     }
 
     public int getX() {
@@ -40,13 +34,12 @@ public class Player {
         return y;
     }
 
-    public static int getWidth() {
-        return width;
+    public static int getWIDTH() {
+        return WIDTH;
     }
 
-    public static int getHeight() {
-        return height;
-    }
-    
+    public static int getHEIGHT() {
+        return HEIGHT;
+    }    
     
 }
